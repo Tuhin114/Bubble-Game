758ec70f1dc548526c80e4da1eacad0d94a53255
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-11-21 18:52:33 +0530

    Added Timer & Hit functionality

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-var clutter = "";
-for (var i = 1; i <= 220; i++) {
-  //To generate random number we use Math.floor(Math.random() * 10 as Math.random generates any random number between 0 & 1 and Math.floor make removes any digites after decimal of any float number
-  clutter += `<div class="bubble">${Math.floor(Math.random() * 10)}</div>`;
+function makeBubble() {
+  var clutter = "";
+  for (var i = 1; i <= 220; i++) {
+    //To generate random number we use Math.floor(Math.random() * 10 as Math.random generates any random number between 0 & 1 and Math.floor make removes any digites after decimal of any float number
+    clutter += `<div class="bubble">${Math.floor(Math.random() * 10)}</div>`;
+  }
+
+  document.querySelector("#pbtm").innerHTML = clutter;
+}
+var timer = 60;
+function runTimer() {
+  var timerClear = setInterval(function () {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timerVal").textContent = timer;
+    } else {
+      clearInterval(timerClear);
+    }
+  }, 1000);
 }
 
-document.querySelector("#pbtm").innerHTML = clutter;
+function getNewHit() {
+  var rn = Math.floor(Math.random() * 10);
+  document.querySelector("#hitVal").textContent = rn;
+}
+
+getNewHit();
+runTimer();
+makeBubble();
